2351d7585612b8fb9c1a7827cd91c95573940c05
Abdur1R <abdurrahman.shaik@phenom.com>
2024-05-04 20:20:45 +0530

    Config and EnableWebMVC Annotations Added!

--- src/main/java/com/sample/sample/controller/UserDetailsController.java
+++ src/main/java/com/sample/sample/controller/UserDetailsController.java
@@ -3,23 +3,27 @@ package com.sample.sample.controller;
 import com.sample.sample.Repository.UserDetailsRepo;
 import com.sample.sample.model.UserDetailsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Optional;
 
+@Configuration
+@EnableWebMvc
 @RestController
 @RequestMapping("/userDetails")
 public class UserDetailsController {
     @Autowired
     UserDetailsRepo userDetailsRepo;
 
-    @CrossOrigin(origins = "*")
+//    @CrossOrigin(origins = "*")
     @PostMapping("/save")
     private UserDetailsDTO postUserData(@RequestBody UserDetailsDTO userDetails){
         return userDetailsRepo.save(userDetails);
     }
 
-    @CrossOrigin(origins = "*")
+//    @CrossOrigin(origins = "*")
     @GetMapping("/getData/{userId}")
     private Optional<UserDetailsDTO> gerUserData(@PathVariable String userId){
         return userDetailsRepo.findById(userId);
