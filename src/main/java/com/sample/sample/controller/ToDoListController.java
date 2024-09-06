48201ac77de1299b79ca3f7667d897dbc2970bd0
Abdur1R <abdurrahmanshaik111@gmail.com>
2024-09-06 00:17:23 -0400

    Delete and Update APIs added for todolist!

--- src/main/java/com/sample/sample/controller/ToDoListController.java
+++ src/main/java/com/sample/sample/controller/ToDoListController.java
@@ -4,6 +4,7 @@ import java.util.*;
 import com.sample.sample.model.ToDoListModel;
 import com.sample.sample.Repository.ToDoListRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,5 +31,20 @@ public class ToDoListController {
     @GetMapping("/getList")
     private List<ToDoListModel> GetList(){
         return toDoListRepo.findAll();
+    }
+
+    @DeleteMapping("/delete/{date}")
+    public List<ToDoListModel> deleteObject(@PathVariable String date) {
+        // Logic to delete the object with the given ID
+        // For example:
+        toDoListRepo.deleteById(date);
+
+        return toDoListRepo.findAll();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Void> UpdateObj(@RequestBody ToDoListModel obj){
+        toDoListRepo.insert(obj);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/sample/sample/model/ToDoListModel.java
+++ src/main/java/com/sample/sample/model/ToDoListModel.java
@@ -1,20 +1,27 @@
 package com.sample.sample.model;
 
+import com.mongodb.lang.Nullable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 //import org.apache.commons.net.ntp.TimeStamp;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
+@Builder
 @Document
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ToDoListModel {
     @Id
-    private Date timeStamp;
+    private String date;
     private String taskTitle;
+    @Nullable
+    @Builder.Default
+    private boolean completed=Boolean.FALSE;
 }
