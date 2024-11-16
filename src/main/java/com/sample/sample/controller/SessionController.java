dbbe71da3926ad76b70d85d256975913a9afc558
Abdur1R <abdurrahmanshaik111@gmail.com>
2024-11-16 16:45:17 -0500

    Password Verification!

--- src/main/java/com/sample/sample/controller/SessionController.java
+++ src/main/java/com/sample/sample/controller/SessionController.java
@@ -1,6 +1,8 @@
 package com.sample.sample.controller;
 
+import com.sample.sample.Repository.UserDetailsRepo;
 import com.sample.sample.model.Session;
+import com.sample.sample.model.UserDetailsDTO;
 import com.sample.sample.service.SessionService;
 import com.sample.sample.service.SessionControlService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +22,27 @@ public class SessionController {
     @Autowired
     private SessionControlService sessionControlService;
 
+    @Autowired
+    private UserDetailsRepo userDetailsRepo;
+
+    @CrossOrigin(origins = "https://abdur1r.github.io")
     @GetMapping("/active")
     public boolean getActiveSessions(@RequestParam String userId) {
         return sessionService.getUserSessions(userId);
     }
 
+    @CrossOrigin(origins = "https://abdur1r.github.io")
     @PostMapping("/login")
-    public ResponseEntity<String> login(HttpSession session, @RequestParam String userId) {
+    public ResponseEntity<String> login(HttpSession session, @RequestParam String userId, @RequestParam String password) {
         // Enforce session limit on login
 //        sessionControlService.enforceSessionLimit(userId);
+        UserDetailsDTO requiredUser=userDetailsRepo.findByEmailId(userId);
+        System.out.println("requiredUser "+requiredUser);
+        if(!(requiredUser!=null && requiredUser.getPassword().equals(password))){
+            return ResponseEntity.ok("Invalid Credentials");
+        }
         sessionService.setSession(userId);
         session.setAttribute("userId", userId);
-        return ResponseEntity.ok("User logged in with session ID: " + session.getId());
+        return ResponseEntity.ok("User logged in with session ID: " + userId);
     }
 }
